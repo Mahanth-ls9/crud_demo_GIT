16f533d312cde4b260a4fc9a8f06d8be0b18ba1f
Mahanth-ls9 <mahanth399@gmail.com>
2023-07-25 23:15:11 +0530

    Process initiation forcreating brances and understanding git commands

---
+++ src/main/java/com/prac/crud_demo_GIT/CrudDemoGitApplication.java
@@ -0,0 +1,13 @@
+package com.prac.crud_demo_GIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prac/crud_demo_GIT/CrudDemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.crud_demo_GIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
